dc54a8835178a3edb6563c52b261ae625051a95b
Prasad Nirmal <113760595+Prasad-root@users.noreply.github.com>
2024-11-25 00:21:46 +0530

    removeAPIkey settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3#ka0&+i*w)gd3if0%2e8)+u(^63^6tk3w6!dyim90-t!-svcl"
+SECRET_KEY = "####################################################"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
